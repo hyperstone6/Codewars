00698e4da32530488f4eaaa435546825a4aec7e6
Amon Ra II <palebluedot85@gmil.com>
2023-01-26 12:28:50 -0500

    add new challenges

--- 8kyu/filterOutTheGeese.js
+++ 8kyu/filterOutTheGeese.js
@@ -1,7 +1,6 @@
 // Write a function that takes a list of strings as an argument and 
 //returns a filtered list containing the same elements but with the 'geese' removed.
 
-
 // My solutions
 
 // solution 1 -- filter method
